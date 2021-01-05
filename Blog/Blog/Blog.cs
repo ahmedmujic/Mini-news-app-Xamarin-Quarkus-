dc490160039ada801dbef562d031ccc8bd182f32
User <ahmed.mujic2018@size.ba>
2021-01-05 19:44:53 +0100

    Add project files.

---
+++ Blog/Blog/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog
+{
+    class Blog
+    {
+        public string NewsImageUrl { get; set; }
+        public string Title { get; set; }
+        public string description { get; set; }
+        //public DateTime date { get; set; }
+    }
+}
---
+++ Blog/Blog/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Blog
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Blog>Blogs { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Blogs = new List<Blog>(); 
+        }
+    }
+}
